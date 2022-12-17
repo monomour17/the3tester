07cdebafa9246a8290901136b8a37b6ba3ba7c68
Ali Rıza <96866572+monomour17@users.noreply.github.com>
2022-12-17 17:52:02 +0300

    Update the3tester.py

--- the3tester.py
+++ the3tester.py
@@ -1,69 +1,31 @@
-Pt = (["AXA", "XAY"],
-    ["AXA", "XAZ"],
-    ["ayz", "cba"],
-    ["11111111", "11111111"],
-    ["XAX", "XXA"],
-    ["XAA", "AXx"],
-    ["zya", "cba"],
-    ["zya", "abc"],
-    ["ayz", "cba"],
-    ["abcd", "zya'"],
-    ["ariby", "#Axaz"],
-    ["123", "111"],
-    ["111", "456"],
-    ["111", "654"],
-    ["151", "161"],
-    ["1111", "4321"],
-    ["kjih098","gefdcba"])
+images=open("the3I.txt")
+results=open("the3R.txt")
+patterns=open("the3P.txt")
+image=images.read().splitlines()
+result=results.read().splitlines()
+pattern=patterns.read().splitlines()
+imagelist=[]
+resultlist=[]
+patternlist=[]
+for i in range(len(image)):
+    imagelist.append(eval(image[i]))
+    resultlist.append(eval(result[i]))
+    patternlist.append(eval(pattern[i]))
 
-It = (["tuz<abcd", ">#sAY#at", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["tuz<abcd", ">#sAY#at", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["tuz<abcd", ">#sAY#at", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["tuz<abcd", ">#sAY#at", "uzyXAAr."],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["AXAXAXAX", "AXAAAXXA", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["tuz<abcd", ">#sAY#at", "uzyXAAr.", "r,lAXxio", "z#a!yabc", "yazy?zya"],
-    ["111111","123456","131111"],
-    ["111111","123456","131111"],
-    ["111111","123456","131111"],
-    ["111111","123456","131111"],
-    ["111111","123456","131111"],
-    ["1234567","abcdfeg","890hijk"])
 
-Rt= ((1, 3, 270),
-    False,
-    (4,5,180),
-    False,
-    (0, 5, 0),
-    (2, 3, 0),
-    False,
-    False,
-    (4,5,180),
-    False,
-    (1,5,90),
-    (0,0,90),
-    (0,3,0),
-    (1,3,180),
-    (0,4,270),
-    False,
-    (1,0,180))
+
+"""KODUNUZU BU ARAYA YAPIŞTIRIN. fonksiyonunuzun isminin "pattern_search" olmasına dikkat edin.
+the3Itxt the3R.txt the3P.txt the3tester.py dosyalarının aynı konumda olduğundan emin olun
+vscode, pycharm gibi programlar bazı kişilerde /home üzerinden çalıştığı için dosyaları okumayıp hata verebilir. 
+o durumlarda bu dosyayı terminalde çalıştırın"""
 
 
 
 
-
-"""KODUNUZU BU ARAYA YAPIŞTIRIN. fonksiyonunuzun isminin "pattern_search" olmasına dikkat edin"""
-
-
-
-
-
-for x in range(len(Pt)):
-    if pattern_search(Pt[x], It[x])==Rt[x]:
+uzunluk=len(imagelist)
+print(f"toplamda {uzunluk} kadar liste vardır. bazı listeler uzun olduğu için bekletebilir, erken kapatmayın.")
+for x in range(len(imagelist)):
+    if pattern_search(patternlist[x], imagelist[x])==resultlist[x]:
         print(x+1,"doğru")
     else:
-        print(x+1,"yanlış, senin cevabın:", pattern_search(Pt[x], It[x])," doğru cevap:",Rt[x] )
+        print(x+1,"yanlış, senin cevabın:", pattern_search(patternlist[x], imagelist[x])," doğru cevap:",resultlist[x] )
